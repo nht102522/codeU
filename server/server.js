c2463a34003ce8174e94a557415984eb3202642c
Nam Trinh <nht35@drexel.edu>
2025-11-19 19:55:00 -0500

    update authentication

--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ const allowedOrigins = [
   process.env.CLIENT_URL ?? "http://localhost:5173",
   "http://localhost:5174",
   "http://127.0.0.1:5173",
+  "https://codeu.onrender.com",
 ];
 
 app.use(
